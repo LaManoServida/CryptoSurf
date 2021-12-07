29e375dc3eac1f4d9924585567b65c51a183d9f9
Andoni Angulo <andoni7@outlook.com>
2021-12-07 02:12:03 +0100

    add arbitrary candlestick number/limit

--- create_dataset.py
+++ create_dataset.py
@@ -1,31 +1,56 @@
 import argparse
+from datetime import datetime
 
 import pandas as pd
 from binance import Client
+from dateutil.relativedelta import relativedelta
 
 from keys import api_key, api_secret
 
 
-def main(args):
-    """Download candle history and save it to csv"""
-    client = Client(api_key, api_secret)  # TODO: support key file
-    candles = client.get_klines(symbol=args.symbol, interval=args.interval, limit=args.limit)
+def calculate_start_time(interval, limit):
+    # split interval into coeficient and unit (for example: '30m' --> 30, 'm')
+    interval_coef, interval_unit = int(interval[:-1]), interval[-1]
 
-    # get columns of interest and set type
+    # total units of time
+    num_units = interval_coef * limit + 1  # +1 because the most recent one is never complete
+
+    # calculate time span of data
+    time_interval = {
+        'm': relativedelta(minutes=num_units),
+        'h': relativedelta(hours=num_units),
+        'd': relativedelta(days=num_units),
+        'w': relativedelta(weeks=num_units),
+        'M': relativedelta(months=num_units)
+    }[interval_unit]
+
+    # convert it to timestamp
+    start_time = (datetime.now() - time_interval).timestamp()
+    start_time = round(start_time * 1000)
+
+    return start_time
+
+
+def main(args):
+    """Download candlestick history and save it to csv"""
+
+    # calculate start time
+    start_time = calculate_start_time(args.interval, args.limit)
+
+    # create client
+    client = Client(api_key, api_secret)  # TODO: support key file
+
+    # download candlestick data (exclude the last one as is not complete yet)
+    candles = client.get_historical_klines(symbol=args.symbol, interval=args.interval, start_str=start_time)[:-1]
+
+    # get columns of interest and set data type
     candles = pd.DataFrame([candle[1:5] for candle in candles], columns=['open', 'high', 'low', 'close'], dtype=float)
 
     # save it to csv
     candles.to_csv('data.csv', index=False)  # TODO: support custom csv path
 
 
-if __name__ == '__main__':  # TODO: support custom start time
-    def parse_limit(limit):
-        limit = int(limit)
-        if 0 < limit <= 1000:
-            return limit
-        raise ValueError
-
-
+if __name__ == '__main__':
     def get_interval_choices():
         return [Client.KLINE_INTERVAL_1MINUTE, Client.KLINE_INTERVAL_3MINUTE, Client.KLINE_INTERVAL_5MINUTE,
                 Client.KLINE_INTERVAL_15MINUTE, Client.KLINE_INTERVAL_30MINUTE, Client.KLINE_INTERVAL_1HOUR,
@@ -34,11 +59,12 @@ if __name__ == '__main__':  # TODO: support custom start time
                 Client.KLINE_INTERVAL_3DAY, Client.KLINE_INTERVAL_1WEEK, Client.KLINE_INTERVAL_1MONTH]
 
 
-    parser = argparse.ArgumentParser(description='Generates the dataset of candelstick historical data.',
+    parser = argparse.ArgumentParser(description='Generates the dataset of latest candelstick historical data.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('symbol', help='the currency pair')
     parser.add_argument('-i', '--interval', default=Client.KLINE_INTERVAL_30MINUTE, choices=get_interval_choices(),
                         help='duration of each candlestick')
-    parser.add_argument('-l', '--limit', default=1000, type=parse_limit, help='number of candlesticks')
+    # TODO change "Limit" to "num" or "number"
+    parser.add_argument('-l', '--limit', default=1000, type=int, help='number of last candlesticks')
 
     main(parser.parse_args())
