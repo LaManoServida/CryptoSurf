8ba2953779b1dfcf5c4080903ad1b8ba40ce93c9
Andoni Angulo <andoni7@outlook.com>
2024-05-14 00:42:54 +0200

    Add script to transform the raw dataset

---
+++ 1-transform_into_sliding_windows.py
@@ -0,0 +1,67 @@
+import argparse
+import os.path
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from config import default_dataset_directory
+
+
+def transform_into_sliding_windows(raw_dataset_path, x_window_size, forecast_window_size, threshold_percentage,
+                                   window_gap, stride, output_dataset_directory=default_dataset_directory):
+    # read dataframe
+    df = pd.read_csv(raw_dataset_path)
+
+    # get its numpy array representation
+    df_array = df.values
+
+    # initialize the lists
+    x_windows_list = []
+    up_list = []
+    down_list = []
+
+    # iterate over the dataset
+    for i in range(0, len(df) - (x_window_size + window_gap + forecast_window_size), stride):
+        # append new X window
+        x_window = df_array[i:i + x_window_size]
+        x_windows_list.append(x_window)
+
+        # append new classes
+        forecast_window = df_array[i + x_window_size + window_gap:i + x_window_size + window_gap + forecast_window_size]
+        up_list.append(np.any(forecast_window > x_window[-1] + x_window[-1] * threshold_percentage / 100))
+        down_list.append(np.any(forecast_window < x_window[-1] - x_window[-1] * threshold_percentage / 100))
+
+    # save the data
+    output_filename = os.path.splitext(os.path.basename(raw_dataset_path))[0].replace('candles', 'windows')
+    output_file_path = os.path.join(output_dataset_directory, output_filename + '.hdf5')
+    with h5py.File(output_file_path, 'w') as f:
+        x_dataset = f.create_dataset('x', data=np.array(x_windows_list))
+        x_dataset.attrs['columns'] = df.columns.tolist()
+        f.create_dataset('up', data=np.array(up_list))
+        f.create_dataset('down', data=np.array(down_list))
+
+    return output_file_path
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Transforms a raw dataset into sliding windows, calculating two '
+                                                 'boolean classes "up" and "down", which are true if any point in the '
+                                                 'forecast window goes up or down, respectively, past the given '
+                                                 'threshold, with respect to the last point of the "X" window. If no '
+                                                 'point exceeds the threshold, "y" is 0',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('raw_dataset_path', help='path of the raw dataset')
+    parser.add_argument('x_window_size', default=100, type=int, help='size of the "X" window')
+    parser.add_argument('forecast_window_size', default=5, type=int,
+                        help='size of the forecast window used to compute the classes')
+    parser.add_argument('threshold_percentage', type=float,
+                        help='percentage of the last point of the "X" window, which is added and substracted to make '
+                             'the positive and negative thresholds')
+    parser.add_argument('-g', '--window-gap', default=0, type=int, dest='window_gap',
+                        help='number of timesteps between "X" window and forecast window')
+    parser.add_argument('-s', '--stride', default=1, type=int, help='stride of the sliding windows')
+    parser.add_argument('--output-dataset-directory', default=default_dataset_directory,
+                        help='destionation of the downloaded dataset', dest='output_dataset_directory')
+
+    transform_into_sliding_windows(**vars(parser.parse_args()))
