29dbbe31a31f9a8075a076526e73db699e211916
Andoni <andoni7@outlook.com>
2024-07-01 22:56:32 +0200

    Discard close_time too

--- dataset_utils.py
+++ dataset_utils.py
@@ -32,7 +32,7 @@ def download_raw_dataset(symbol, interval, start_timestamp_millis, end_timestamp
         candles,
         columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_asset_volume',
                  'number_of_trades', 'taker_buy_base_asset_volume', 'taker_buy_quote_asset_volume', 'ignore']
-    ).drop('ignore', axis=1).apply(pd.to_numeric)
+    ).drop(['close_time', 'ignore'], axis=1).apply(pd.to_numeric)
 
     return df
 
